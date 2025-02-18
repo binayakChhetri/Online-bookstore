a8133b6287aade830142bec8c4e24f414796e7cf
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-02-18 15:33:11 +0545

    Completed designing signup page

--- signup.php
+++ signup.php
@@ -1,60 +1,67 @@
 <?php
-	$title = "User SignUp";
-	require_once "./template/header.php";
+$title = "User SignUp";
+require_once "./template/header.php";
 ?>
 
-	<form class="form-horizontal" method="post" action="user_signup.php">
-    <div class="form-group">
-        <label for="exampleInputEmail1">Firstname</label>
-        <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Firstname" name="firstname">
-    </div>
-    <div class="form-group">
-        <label for="exampleInputEmail1">Lastname</label>
-        <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Lastname" name="lastname">
-    </div>
-    <div class="form-group">
+<style>
+  <?php include './CSS/signup.css'; ?>
+</style>
 
-        <label for="inputEmail4">Email</label>
-        <input type="text" class="form-control" id="inputEmail4" placeholder="Email" name="email">
+<div class="signup-container">
+  <h2 class="title">Registration</h2>
+  <div class="content">
+    <form action="user_signup.php" method="post">
+      <div class="user-details">
+        <div class="input-box">
+          <label class="details">First Name</label>
+          <input type="text" placeholder="Enter your firstname" required />
         </div>
-        <div class="form-group">
-        <label for="inputPassword4">Password</label>
-        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" name="password">
-    </div>
-    <div class="form-group">
-        <label for="inputAddress">Address</label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" name="address">
-    </div>
-    <div class="form-row">
-        <div class="form-group col-md-4">
-        <label for="inputCity">City</label>
-        <input type="text" class="form-control" id="inputCity" name="city">
+        <div class="input-box">
+          <label class="details">Last Name</label>
+          <input type="text" placeholder="Enter your lastname" required />
         </div>
-        <div class="form-group col-md-2">
+        <div class="input-box">
+          <label class="details">Email</label>
+          <input type="email" placeholder="Enter your email" required />
         </div>
-        <div class="form-group col-md-4">
-        <label for="inputZip">Zip</label>
-        <input type="text" class="form-control" id="inputZip" name="zipcode">
+        <div class="input-box">
+          <label class="details">Password</label>
+          <input type="password" placeholder="Enter your password" required />
         </div>
-    </div>
-    <div class="form-group col-md-12">
-    <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-</form>
-<div style="position:fixed; bottom:120px">
+        <div class="input-box">
+          <label class="details">Address</label>
+          <input type="text" placeholder="Enter your address" required />
+        </div>
+        <div class="input-box">
+          <label class="details">City</label>
+          <input type="text" placeholder="Enter your city name" required />
+        </div>
+        <div class="input-box">
+          <label class="details">Zip code</label>
+          <input type="number" placeholder="" required />
+        </div>
+      </div>
+      <div class="button">
+        <input type="submit" value="Register" />
+      </div>
+    </form>
+  </div>
+</div>
+
+
 
 <?php
-    $fullurl="http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    if(strpos($fullurl,"signup=empty")==true){
-        echo '<P style="color:red">You did not fill in all the fields.</P>';
-        exit();
-    }
-    if(strpos($fullurl,"signup=invalidemail")==true){
-        echo '<P style="color:red">You did not enter a valid email address.</P>';
-        exit();
-    }
+$fullurl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+if (strpos($fullurl, "signup=empty") == true) {
+  echo '<P style="color:red">You did not fill in all the fields.</P>';
+  exit();
+}
+if (strpos($fullurl, "signup=invalidemail") == true) {
+  echo '<P style="color:red">You did not enter a valid email address.</P>';
+  exit();
+}
 ?>
 </div>
 <?php
-	require_once "./template/footer.php";
+require_once "./template/footer.php";
 ?>
